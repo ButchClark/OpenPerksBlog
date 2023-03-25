e8a0d69b3151aa0027c06942cebcfce0010f58eb
butchclark <butch.clark@hushmail.com>
2023-03-25 17:48:48 -0600

    Updating to blog format

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Open Perks project website';
+export const SITE_DESCRIPTION = 'Welcome to the Open Perks website, where we talk about web3-based customer rewards!';
